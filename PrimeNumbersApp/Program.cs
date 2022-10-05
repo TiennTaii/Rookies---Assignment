2f0e6a781a5f4999193079eb1613ad101cc1516b
taipt <phamtientai0311@gmail.com>
2022-10-06 01:14:13 +0700

    complete

---
+++ PrimeNumbersApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PrimeNumbersApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true;
+            int i, j;
+
+            Console.WriteLine("Prime Numbers : ");
+
+            for (i = 2; i <= 100; i++) // 0 and 1 not prime number!
+            {
+                for (j = 2; j <= 100; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine(i);
+                }
+
+                isPrime = true;
+            }
+        }
+    }
+}
