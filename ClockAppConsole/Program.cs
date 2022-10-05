e731dad8380606b145f0c2d20c0e4e27f50192b9
taipt <phamtientai0311@gmail.com>
2022-10-05 14:07:42 +0700

    complete first

---
+++ ClockAppConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Timers;
+
+namespace ClockAppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
+            Console.ReadKey();
+
+            timer.Stop();
+        }
+
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
