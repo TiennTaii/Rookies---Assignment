44f8932f8775550f326aab4cb24fe26fd960c279
taipt <phamtientai0311@gmail.com>
2022-10-22 16:01:33 +0700

    ...

--- Entity/assignment_two/Program.cs
+++ Entity/assignment_two/Program.cs
@@ -1,5 +1,9 @@
-
 using assignment_two.Data;
+using assignment_two.Repositories;
+using assignment_two.Reposotories;
+using assignment_two.Reposotories.Interfaces;
+using assignment_two.Services;
+using assignment_two.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +13,14 @@ builder.Services.AddDbContext<ProductDataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnString")));
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+builder.Services.AddScoped<IProductService, ProductService>();
+
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
